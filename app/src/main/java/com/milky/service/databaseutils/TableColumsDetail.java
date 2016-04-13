4b943911b00f7e7acfedf9e8ff53f82ab94db289
NehaSharma_Sensation <neha.sharma@sensationsolutions.com>
2016-04-13 12:01:47 +0530

    New columns added to tables Isdeleted,Datemodified,dirty

--- app/src/main/java/com/milky/service/databaseutils/TableColumsDetail.java
+++ app/src/main/java/com/milky/service/databaseutils/TableColumsDetail.java
@@ -10,35 +10,39 @@ public class TableColumsDetail {
             + TableColumns.DateModified + " DATETIME,"+ TableColumns.DateAdded + " DATETIME," + TableColumns.FirstName + " TEXT,"
             + TableColumns.LastName + " TEXT," + TableColumns.Mobile + " TEXT,"
             + TableColumns.EndDate + " DATETIME," + TableColumns.TotalSms + " INTEGER," + TableColumns.UsedSms + " INTEGER,"
-            + TableColumns.Validated + " INTEGER," + TableColumns.ServerAccountId + " TEXT" + ")";
+            + TableColumns.Validated + " INTEGER," + TableColumns.ServerAccountId + " TEXT,"+TableColumns.IsDeleted+" INTEGER,"
+            +TableColumns.Dirty+" INTEGER"+ ")";
 
     //Global Settings
     public static final String GLOBAL_ETTINGS = "CREATE TABLE " + TableNames.GlobalSetting + "("
             + TableColumns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + TableColumns.DefaultRate + " REAL,"
-            + TableColumns.TAX + " REAL," + TableColumns.RollDate + " DATETIME" + ")";
+            + TableColumns.TAX + " REAL," + TableColumns.RollDate + " DATETIME,"+TableColumns.IsDeleted+" INTEGER,"
+            +TableColumns.Dirty+" INTEGER,"+TableColumns.DateModified+" DATETIME" + ")";
 
     //AREA
     public static final String AREA = "CREATE TABLE " + TableNames.AREA + "(" + TableColumns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"
-            + TableColumns.Locality + " TEXT,"+TableColumns.Name+" TEXT," + TableColumns.City + " TEXT," +  TableColumns.Dirty + " TEXT" + ")";
+            + TableColumns.Locality + " TEXT,"+TableColumns.Name+" TEXT," + TableColumns.City + " TEXT," +  TableColumns.Dirty + " TEXT,"+TableColumns.IsDeleted+" INTEGER,"
+            +TableColumns.DateModified+" DATETIME" + ")";
 
     //CUSTOMER
     public static final String CUSTOMER = "CREATE TABLE " + TableNames.CUSTOMER + "(" + TableColumns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + TableColumns.FirstName + " TEXT,"
             + TableColumns.LastName + " TEXT," + TableColumns.Mobile + " TEXT," + TableColumns.Address1 + " TEXT," + TableColumns.Address2 + " TEXT," + TableColumns.Balance + " REAL,"
-            + TableColumns.DateAdded + " DATETIME," + TableColumns.StartDate + " DATETIME," + TableColumns.AreaId + " INTEGER,"+ TableColumns.DeletedOn + " INTEGER," + TableColumns.DateModified + " DATETIME,"
-            + TableColumns.IsDeleted + " INTEGER," + TableColumns.Dirty + " INTEGER" + " ,FOREIGN KEY (" + TableColumns.AreaId + ") REFERENCES " + TableNames.AREA + "(" + TableColumns.ID + ")" +
+            + TableColumns.DateAdded + " DATETIME," + TableColumns.StartDate + " DATETIME," + TableColumns.AreaId + " INTEGER,"+ TableColumns.DeletedOn + " INTEGER," +TableColumns.IsDeleted+" INTEGER,"
+            +TableColumns.Dirty+" INTEGER,"+TableColumns.DateModified+" DATETIME" + " ,FOREIGN KEY (" + TableColumns.AreaId + ") REFERENCES " + TableNames.AREA + "(" + TableColumns.ID + ")" +
             ")";
 
     //CUSTOMER"S BILL
     public static final String CUSTOMERS_BILL = "CREATE TABLE " + TableNames.Bill + "(" + TableColumns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," + TableColumns.StartDate + " DATETIME," + TableColumns.EndDate + " DATETIME,"
             + TableColumns.TotalQuantity + " REAL," +  TableColumns.Rate + " REAL," + TableColumns.Balance + " REAL," + TableColumns.Adjustment + " REAL," + TableColumns.TAX + " REAL," + TableColumns.IsCleared + " INTEGER,"
-            + TableColumns.PaymentMade + " REAL," + TableColumns.IsOutstanding + " INTEGER," + TableColumns.TotalAmount + " REAL," + TableColumns.DateAdded + " DATETIME,"+TableColumns.DateModified + " DATETIME," + TableColumns.Dirty + " INTEGER," + TableColumns.CustomerId + " INTEGER,"
-            + " FOREIGN KEY (" + TableColumns.CustomerId + ")" + " REFERENCES " + TableNames.CUSTOMER + "(" + TableColumns.ID + ")" + ")";
+            + TableColumns.PaymentMade + " REAL," + TableColumns.IsOutstanding + " INTEGER," + TableColumns.TotalAmount + " REAL," + TableColumns.DateAdded + " DATETIME,"+ TableColumns.CustomerId + " INTEGER,"+TableColumns.IsDeleted+" INTEGER,"
+            +TableColumns.Dirty+" INTEGER,"+TableColumns.DateModified+" DATETIME,"  + " FOREIGN KEY (" + TableColumns.CustomerId + ")" + " REFERENCES " + TableNames.CUSTOMER + "(" + TableColumns.ID + ")" + ")";
 
     //Customers Setting
     public static final String CUSTOMER_SETTINGS = "CREATE TABLE " + TableNames.CustomerSetting + "(" + TableColumns.ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
             TableColumns.DefaultRate + " REAL," + TableColumns.DefaultQuantity + " TEXT,"
             + TableColumns.Balance + " REAL," + TableColumns.StartDate + " DATETIME," + TableColumns.EndDate + " DATETIME,"
-            + TableColumns.IsCustomDelivery + " INTEGER," + TableColumns.Dirty + " INTEGER," + TableColumns.CustomerId + " INTEGER,"
+            + TableColumns.IsCustomDelivery + " INTEGER," + TableColumns.CustomerId + " INTEGER,"+TableColumns.IsDeleted+" INTEGER,"
+            +TableColumns.Dirty+" INTEGER,"+TableColumns.DateModified+" DATETIME,"
             + " FOREIGN KEY (" + TableColumns.CustomerId + ")" + " REFERENCES " + TableNames.CUSTOMER + "(" + TableColumns.ID + ")" + ")";
 
 //    //DELIVERY
